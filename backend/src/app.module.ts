fe27b8f0ee9486fb04bd31a5debc76bff105f6e8
Lenny Garnier <lenny.garnier00@gmail.com>
2026-01-13 00:42:26 +0100

    created backend and frontend folders

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,6 +22,7 @@ import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { GithubService } from './github/github.service';
 
 @Catch(HttpException)
 class HttpExceptionFilter extends BaseExceptionFilter {
@@ -63,6 +64,7 @@ class HttpExceptionFilter extends BaseExceptionFilter {
       useClass: HttpExceptionFilter,
     },
     UserService,
+    GithubService,
   ],
 })
 export class AppModule {}
